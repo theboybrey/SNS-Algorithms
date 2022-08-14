01f0f2940b02d1c0f615415146ceb097652f0919
Lex Benjamin <abessilfie@st.ug.edu.gh>
2022-08-14 17:01:07 +0000

    A

--- sort.java
+++ sort.java
@@ -10,8 +10,20 @@ class Sort
 
         int i = 0, j = 0;
         for (i = 0; i < low; i++){
-            L[i] = arr[left + i];
+            L[i] = arr[left + i];    
+        }
+
+        for(j = 0; j < high; j++){
+            R[j] = arr[middle + 1 +j];
+        }
+
+        int k = left;
+        i = 0;
+        j = 0;
+
+        while (i <low && j < high){
             
         }
+
     }
 }
