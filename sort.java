93747a2ee674e20f00eb3129a28a11f266411473
Lex Benjamin <abessilfie@st.ug.edu.gh>
2022-08-14 16:46:09 +0000

    Initial SNS Commit

---
+++ sort.java
@@ -0,0 +1,11 @@
+class Sort
+{
+    void merge(int arr[], int left, int middle, int right)
+    {
+        int low = middle - left + 1;
+        int high = right - middle;
+
+        int L[] = new int[low];
+        int R[] = new int[high];
+    }
+}
